d9c5f03b025e93d0c5f421133519f18bb6e139e6
atommnava <atommunoznava@gmail.com>
2025-04-18 20:39:55 -0600

    Laberinto creación

--- src/computacion/cuantica/main/LaberintoCuantico.java
+++ src/computacion/cuantica/main/LaberintoCuantico.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class LaberintoCuantico {
-    // Variables
+    // Variables DE ENTORNO
     public int contador = 0;
     public char[][] laberinto = {
             {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
